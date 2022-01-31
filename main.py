2e3dc1da086ce4d0e07083f668792fdf3ed06b66
Nikolay.Nikolov2 <nikolay.nikolov2@adastragrp.com>
2022-01-31 16:28:48 +0200

    final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 counter = 0
-for c in range(1,48):
-    for b in range (1, c):
+for c in range(1, 48):
+    for b in range(1, c):
         for a in range(1, b):
             if a * a + b * b == c * c:
                 counter += 1
